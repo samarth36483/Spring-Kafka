3fd2b7a60f4a2a7cf097f21d4e80608979b8c7ae
samarth36483 <raipuriyasamarth@gmail.com>
2024-01-17 12:41:48 +0530

    created orderproducer

---
+++ src/main/java/com/samarth/kafka/orderproducer/App.java
@@ -0,0 +1,13 @@
+package com.samarth.kafka.orderproducer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+  
+    }
+}
---
+++ src/main/java/com/samarth/kafka/orderproducer/OrderProducer.java
@@ -0,0 +1,32 @@
+package com.samarth.kafka.orderproducer;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class OrderProducer {
+
+	public static void main(String[] args) {
+		Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+        
+        KafkaProducer<String, Integer> producer = new KafkaProducer<>(properties);
+        ProducerRecord<String, Integer> record = new ProducerRecord<>("OrderTopic", "Mac Book Pro", 10);
+        
+        try {
+        	producer.send(record);
+        	System.out.println("Message sent successfully");
+        }
+        catch(Exception e) {
+        	e.printStackTrace();
+        }
+        finally {
+        	producer.close();
+        }
+
+	}
+
+}
